59433b363c7e85d2e4165a179fe2028b43c3e701
Narimannmn <138319759+Narimannmn@users.noreply.github.com>
2023-11-29 14:48:24 +0600

    sd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Routes, RouterOutlet } from '@angular/router';
+import {MainViewComponent} from "./pages/main-view/main-view.component";
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {BudgetComponent} from "./pages/budget/budget.component";
 
+//const routes : Routes = [
+  //{ path : '', component: MainViewComponent}
+//]
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainViewComponent, DragDropModule, BudgetComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
